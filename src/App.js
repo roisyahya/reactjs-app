271d76aee9bb62aa82e4a4ca3ec7b9d0cfa2d015
roisyahya <roisyahya@gmail.com>
2021-03-21 12:32:24 +0700

    V1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "assets/scss/style.scss";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Hai dunia
+    </div>
+  );
+}
+
+export default App;
